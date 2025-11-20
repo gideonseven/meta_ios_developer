1e92614bee02ea70e81b93ad7c750273f3d62e8e
Gideon Tobing <gideon.tobing@gmail.com>
2025-11-20 12:32:56 +1000

    add async protocol

---
+++ meta_working_with_data_ios/meta_working_with_data_ios/m2/URLSchema.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//usage of URL components
+var components = URLComponents()
+components.scheme = "https"
+components.host = "google.com"
+components.path = "/images"
+
+//URL SessionDownloadTask
+func downloadTask(
+    with request: URLRequest,
+    completionHandler: @escaping @Sendable (URL?, URLResponse?, Error?) -> Void
+) -> URLSessionDownloadTask
+
+
+
+downloadTask.resume()
+downloadTask.cancel()
