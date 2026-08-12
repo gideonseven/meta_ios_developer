288dd8c227f989a0048e4671c2dbda7f08f69360
Gideon Tobing <gideon.tobing@gmail.com>
2026-08-12 19:42:59 +0800

    add async call http

---
+++ meta_working_with_data_ios/m2/HTTPProtocol.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let menuAddress = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
+
+let menuURL = URL(string: menuAddress)
+
+guard let menuURL = menuURL else {
+    throw NSError()
+}
+
+let request = URLRequest(url: menuURL)
+
+let task = URLSession.shared.dataTask(with: menuURL){
+    data, response, error in
+    if let data = data,
+       let responseString = String(data: data,
+                                   encoding: .utf8){
+        print(responseString)
+    }
+}
+
+task.resume()
